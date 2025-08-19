0bec0e038418de870da671b8c580713455da3c12
Thiago <thiagoruan46@gmail.com>
2025-08-19 10:28:30 -0300

    Update: keymaps.lua New: snacks.lua - Added split navigation & new file for snacks

--- lua/Thy/core/keymaps.lua
+++ lua/Thy/core/keymaps.lua
@@ -5,8 +5,8 @@ vim.g.maplocalleader = " "
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection"})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection"})
-
 vim.keymap.set("n", "J", "mzJ`z")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
 
@@ -22,7 +22,8 @@ vim.keymap.set("v", "p", '"_dp', opts)
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-vim.keymap.set("i", "<C-C>", "<Esc>")
+vim.keymap.set({ "n", "v", "i"}, "<C-s>", ":w<CR>")
+vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
@@ -42,13 +43,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Tabs Control
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>")
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>")
-vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>")
-vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>")
-vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>")
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>") -- New empty tab
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") -- Copy current tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") -- Close current tab
+vim.keymap.set("n", "<TAB>", "<cmd>tabn<CR>") -- Go to next tab
+vim.keymap.set("n", "<leader><TAB>", "<cmd>tabp<CR>") -- Go to prev tab
 
 -- Split Control
+vim.keymap.set("n", "<C-h>", "<C-w>h") -- "move to left split"
+vim.keymap.set("n", "<C-j>", "<C-w>j") -- "move to down split"
+vim.keymap.set("n", "<C-k>", "<C-w>k") -- "move to up split"
+vim.keymap.set("n", "<C-l>", "<C-w>l") -- "move to right split"
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
 vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
---
+++ lua/Thy/plugins/snacks.lua
@@ -0,0 +1,6 @@
+return {
+    {
+        "folke/snacks.vim"
+        
+   }
+}
